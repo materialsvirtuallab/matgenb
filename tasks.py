2caaa22b4217274a806c8477699ae10f5756602f
Shyue Ping Ong <shyuep@gmail.com>
2024-05-15 07:14:57 -0700

    Add FHI Aims example.

--- tasks.py
+++ tasks.py
@@ -14,7 +14,7 @@ __date__ = "Sep 1, 2014"
 @task
 def update_html(ctx, year: str = None):
     year = year or f"{datetime.now():%Y}"
-    ctx.run(f"rm docs/_posts/{year}-*.html")
+    ctx.run(f"rm docs/_posts/{year}-*.html", warn=True)
     ctx.run(
         f"jupyter nbconvert --to html notebooks/{year}-*.ipynb --output-dir "
         "docs/_posts"
